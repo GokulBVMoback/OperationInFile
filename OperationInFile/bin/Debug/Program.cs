cd05fa39dcaafaa6d2741d5c812e507b80b94c0a
GokulBVMoback <113531158+GokulBVMoback@users.noreply.github.com>
2022-09-30 22:52:53 +0530

    Add files via upload
    Operation In File: Open and Read an Existing File, Add New text the File, Copy the file and Create new file and Delete File

---
+++ OperationInFile/bin/Debug/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationInFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory());//, "Report.txt");
+            Console.WriteLine(path);    
+            //string path = $".\\Report.txt";
+            //using (FileStream fs = File.Open(path, FileMode.Open)) ;
+            //string readText = File.ReadAllText(path);
+            //Console.WriteLine(readText);
+        }
+    }
+}
